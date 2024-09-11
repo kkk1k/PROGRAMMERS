d42d3c54dc188896ae6f44efee16408f2d83f450
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2024-09-11 21:06:51 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.95 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -2,13 +2,13 @@ function solution(n) {
     var answer = [];
 
     for (var i = 0; i < n; i++) {
-        answer[i] = []; // Initialize the inner array
+        answer[i] = []; 
 
         for (var j = 0; j < n; j++) {
             if (i === j) {
-                answer[i][j] = 1; // Assign 1 to the diagonal
+                answer[i][j] = 1; 
             } else {
-                answer[i][j] = 0; // Assign 0 elsewhere
+                answer[i][j] = 0; 
             }
         }
     }
