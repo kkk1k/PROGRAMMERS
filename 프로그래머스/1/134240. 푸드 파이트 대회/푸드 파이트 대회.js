c37995a7b43afee889470354ecf44e8fdc7fb205
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2024-10-18 20:57:19 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,9 +1,10 @@
 function solution(food) {
     var answer = '';
     for (let i = 1 ; i<food.length; i++) {
-        for (j = 0 ; j<Math.floor(food[i]/2); j++) {
-            answer += `${i}` 
-        }
+        // for (j = 0 ; j<Math.floor(food[i]/2); j++) {
+        //     answer += `${i}` 
+        // }
+        answer += `${i}`.repeat(Math.floor(food[i]/2))
         
     }
     return answer + "0" + answer.split('').reverse().join('');
