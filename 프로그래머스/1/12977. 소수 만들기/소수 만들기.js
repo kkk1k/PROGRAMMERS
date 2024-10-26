ce4491a2d77fcd4d3a636d2a742d52a190a4e4da
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2024-10-26 23:54:17 +0900

    [level 1] Title: 소수 만들기, Time: 7.17 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,24 @@
+function solution(nums) {
+    let count = 0;
+
+    // 소수 판별 함수
+    function isPrime(num) {
+        if (num < 2) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    // 세 개의 수 조합을 만들고, 합이 소수인지 확인
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                if (isPrime(sum)) count++;
+            }
+        }
+    }
+
+    return count;
+}
