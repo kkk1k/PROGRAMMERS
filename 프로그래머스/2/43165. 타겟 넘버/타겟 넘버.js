a07ddc29efcbd8247919c6c5b3ffeb0c1145469a
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2024-11-01 22:56:30 +0900

    [level 2] Title: 타겟 넘버, Time: 15.61 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+    var answer = 0;
+    function dfs(index, currentSum) {
+        if(index === numbers.length) {
+            if(currentSum === target) {
+                answer++
+            }
+            return
+        }
+        
+        dfs(index+1, currentSum + numbers[index])
+        dfs(index+1, currentSum - numbers[index])
+    }
+    
+    dfs(0,0)
+    return answer;
+}
