b5ccc52b0da6b3512a2c77ac2c4a1c44f420d136
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2024-11-06 13:54:48 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.52 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -10,42 +10,43 @@ function solution(numbers, hand) {
     // 시작 위치 설정
     let leftThumb = keyPosition['*'];
     let rightThumb = keyPosition['#'];
-    let result = [];
+    let result = '';
 
     // 거리 계산 함수 (맨해튼 거리)
     function distance(pos1, pos2) {
         return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
     }
     
-    // 각 숫자를 순서대로 처리
-    for (let number of numbers) {
-        if ([1, 4, 7].includes(number)) {  // 왼쪽 열
-            result.push('L');
-            leftThumb = keyPosition[number];
-        } else if ([3, 6, 9].includes(number)) {  // 오른쪽 열
-            result.push('R');
-            rightThumb = keyPosition[number];
-        } else {  // 가운데 열
-            const leftDist = distance(leftThumb, keyPosition[number]);
-            const rightDist = distance(rightThumb, keyPosition[number]);
+    numbers.forEach(item => {
+        if([1,4,7].includes(item)) {
+            result += "L"
+            leftThumb = keyPosition[item]
+        } else if ([3,6,9].includes(item)) {
+            result += "R"
+            rightThumb = keyPosition[item]
+        } else {
+            const leftDist = distance(leftThumb, keyPosition[item]);
+            const rightDist = distance(rightThumb, keyPosition[item]);
             
-            if (leftDist < rightDist) {
-                result.push('L');
-                leftThumb = keyPosition[number];
-            } else if (rightDist < leftDist) {
-                result.push('R');
-                rightThumb = keyPosition[number];
-            } else {  // 거리가 같을 경우
-                if (hand === "left") {
-                    result.push('L');
-                    leftThumb = keyPosition[number];
-                } else {
-                    result.push('R');
-                    rightThumb = keyPosition[number];
+            if(leftDist > rightDist) {
+                result += "R"
+                rightThumb = keyPosition[item]
+            } else if (leftDist < rightDist) {
+                result += "L"
+                leftThumb = keyPosition[item]
+            } else {
+                if(hand === "right") {
+                    result += "R"
+                    rightThumb = keyPosition[item]
+                }
+                if(hand === "left") {
+                    result += "L"
+                leftThumb = keyPosition[item]
                 }
             }
+            
         }
-    }
+    })
     
-    return result.join('');
+    return result;
 }
