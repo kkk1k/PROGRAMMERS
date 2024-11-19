685b25cc3da89e28727ff8e8600ff5bcfc2fbf96
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2024-11-19 12:57:26 +0900

    [level 1] Title: 달리기 경주, Time: 165.51 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,25 +1,19 @@
 function solution(players, callings) {
-    // 선수들의 순위를 해시 맵으로 저장
-    const rankMap = new Map();
-    players.forEach((player, index) => {
-        rankMap.set(player, index);
-    });
+    
+    var obj = {}
+    
+    players.forEach((item,idx) => {
+        obj[item] = idx
+    })
+    
+    callings.forEach(item => {
+        const playerIdx = obj[item]
+        const prevPlayer = players[playerIdx - 1];
+        [players[playerIdx - 1], players[playerIdx]] = [players[playerIdx], players[playerIdx - 1]];
+        obj[item]--;
+        obj[prevPlayer]++;
 
-    // callings 배열의 이름에 따라 순위 갱신
-    callings.forEach((calledPlayer) => {
-        const currentRank = rankMap.get(calledPlayer);
-
-        if (currentRank > 0) { // 1등이 아닌 경우만 처리
-            const prevPlayer = players[currentRank - 1];
-
-            // 선수 배열에서 순위 교체
-            [players[currentRank - 1], players[currentRank]] = [players[currentRank], players[currentRank - 1]];
-
-            // 해시 맵에서 순위 갱신
-            rankMap.set(calledPlayer, currentRank - 1);
-            rankMap.set(prevPlayer, currentRank);
-        }
-    });
-
+    })
+    
     return players;
 }
