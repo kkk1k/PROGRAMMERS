b5e70e288e9c2659e3b9d4b2b7ff5ac9e97d68e4
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2024-11-22 17:48:03 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 9.40 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,39 @@
+function solution(friends, gifts) {
+    var answer = Array(friends.length).fill(0);
+    let giftIdx = Array(friends.length).fill(0);
+    let relation = Array.from({ length: friends.length }, () => Array(friends.length).fill(0));
+
+    gifts.forEach(item => {
+        let [give,get] = item.split(" ")
+        let giveIdx = friends.indexOf(give)
+        let getIdx = friends.indexOf(get)
+        relation[giveIdx][getIdx] += 1
+    })
+    
+    for(let i = 0; i<relation.length; i++){
+        for(let j = 0; j<relation.length; j++) {
+            giftIdx[i] += relation[i][j]
+            giftIdx[i] -= relation[j][i]
+        }
+    }
+    
+    for(let i = 0; i<relation.length; i++){
+        for(let j = 0; j<relation.length; j++) {
+            if(i===j) {
+                continue
+            }
+            if(relation[i][j] > relation[j][i]) {
+                answer[i] += 1
+            } else if(relation[i][j]=== relation[j][i]) {
+                if(giftIdx[i] > giftIdx[j]) {
+                    
+                    answer[i] += 1
+                } 
+            }
+        }
+    }
+    
+    return answer.reduce((a,b) => {
+        return a >= b ? a : b
+    },0);
+}
