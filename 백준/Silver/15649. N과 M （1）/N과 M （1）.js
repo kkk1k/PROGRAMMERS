8e6c1aee1929a252110fc1b6dcfd6f31634e5624
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2025-01-13 11:35:16 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 18100 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync( "/dev/stdin" )
+  .toString()
+  .trim()
+  .split(" ");
+
+const N = Number(input[0]);
+const M = Number(input[1]);
+
+const result = [];
+const sequence = [];
+const visited = Array(N + 1).fill(false);
+
+function dfs(depth) {
+  if (depth === M) {
+    result.push(sequence.join(" "));
+    return;
+  }
+  for (let i = 1; i <= N; i++) {
+    if (!visited[i]) {
+      visited[i] = true;
+      sequence.push(i);
+      dfs(depth + 1);
+      sequence.pop();
+      visited[i] = false;
+    }
+  }
+}
+
+dfs(0);
+console.log(result.join("\n"));
