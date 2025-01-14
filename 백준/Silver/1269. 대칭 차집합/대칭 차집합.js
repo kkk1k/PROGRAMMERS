4bcad823794c45604133b759ce528ca7296088bc
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2025-01-14 11:15:07 +0900

    [Silver IV] Title: 대칭 차집합, Time: 320 ms, Memory: 55344 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync( "/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [a, b] = input[0].split(" ").map(Number);
+
+const arrA = input[1].split(" ").map(Number);
+
+const arrB = input[2].split(" ").map(Number);
+
+const hashA = new Set(arrA);
+const intersection = arrB.filter((item) => hashA.has(item));
+
+console.log(arrA.length + arrB.length - intersection.length * 2);
