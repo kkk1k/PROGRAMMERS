ffa241bae9041db89c158c9320beb6d0fde41ac3
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2025-01-22 11:46:36 +0900

    [level 2] Title: 영어 끝말잇기, Time: 3.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,28 +1,31 @@
 function solution(n, words) {
     var answer = [];
-
-    let prev = words[0]
-    let arr = []
+    let obj = {}
     let num = 0
-    for(let i = 0; i<words.length; i++) {
-        if(i % n === 0 ){
-            num += 1
-        } 
-        if(i === 0 ){
-            arr.push(words[i])
-            continue
-        } else if (words[i][0] === prev[prev.length - 1] && !arr.includes(words[i]) ) {
-            arr.push(words[i])
-            prev = words[i]
-        } else if ( words[i][0] !== prev[prev.length - 1] || arr.includes(words[i]) ) {
-            
-            answer.push(i % n + 1)
-            answer.push(num)
+    let ord = 0
+    let lastWord = words[0][0]
+    for(let i = 0; i< words.length; i++) {
+        let word = words[i]
+        if(obj[word] === 1) {
+            num = Math.floor(i / n) +1 
+            ord = i % n + 1
+            console.log(i)
             break
+        } else {
+            if(lastWord !== word[0]) {
+                num = Math.floor(i / n) +1
+                ord = i % n + 1
+                console.log(i)
+                break
+            } else {
+                obj[word] = 1
+                lastWord = word[word.length -1 ]
+            }
         }
-    }
-    if(answer.length === 0 ) {
-        return [0,0]
+        
     } 
+    
+    answer.push(ord)
+    answer.push(num)
     return answer;
 }
