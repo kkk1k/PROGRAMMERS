106ae1c264fbe244482b2c3ecbd17a092ac67dba
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2025-02-06 10:33:38 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,33 +1,21 @@
-function find (parents, x) {
-    if(parents[x] === x) return x
-    parents[x] = find(parents, parents[x])
-    return parents[x]
-}
-
-function union (parents, x,y ) {
-    const rootX = find(parents, x)
-    const rootY = find(parents, y) 
-    if( rootX !== rootY ) {
-        parents[rootX] = rootY
-    }
-}
-
 function solution(n, computers) {
     var answer = 0;
-    const parents = Array.from({length : n}, (_,idx) => idx)
-
-    
-    for(let i = 0; i < computers.length; i++ ) {
-        for(let j = 0; j< n ; j++) {
-            if( computers[i][j] === 1) {
-                union(parents,i,j)
+    const visited = Array(n).fill(false)
+    function dfs (node) {
+        visited[node] = true
+        for(let i = 0; i< computers[node].length; i++) {
+            if(computers[node][i] === 1 && !visited[i]) {
+                dfs(i)
             }
         }
     }
     
-    for (let i = 0; i< parents.length ; i++) {
-        parents[i] = find(parents, i)
+    for (let i = 0; i<n ; i++) {
+        if(!visited[i]) {
+            dfs(i)
+            answer++
+        }
+        
     }
-    console.log(parents)
-    return [... new Set(parents)].length;
+    return answer;
 }
