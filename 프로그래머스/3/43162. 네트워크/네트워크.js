b693d294021ea79cb1525b54361e7b18bef42459
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2025-02-06 10:45:23 +0900

    [level 3] Title: 네트워크, Time: 5.66 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,21 +1,28 @@
 function solution(n, computers) {
     var answer = 0;
-    const visited = Array(n).fill(false)
-    function dfs (node) {
+    const visited = Array(n).fill(false) 
+    function bfs (node) {
+        console.log(node, visited)
         visited[node] = true
-        for(let i = 0; i< computers[node].length; i++) {
-            if(computers[node][i] === 1 && !visited[i]) {
-                dfs(i)
+        
+        const queue = []
+        queue.push(node)
+        while(queue.length > 0) {
+            const item = queue.shift()
+            for (let i = 0 ; i < n ; i++) {
+                if(computers[item][i] === 1 && !visited[i]) {
+                    visited[i] = true
+                    queue.push(i)
+                }
             }
         }
     }
     
-    for (let i = 0; i<n ; i++) {
+    for (let i = 0; i< n; i++) {
         if(!visited[i]) {
-            dfs(i)
+            bfs(i)
             answer++
         }
-        
     }
     return answer;
 }
