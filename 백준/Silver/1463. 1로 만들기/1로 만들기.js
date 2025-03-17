8dcf2d72a5734109269c8719a11f291df7623a44
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2025-03-17 09:48:49 +0900

    [Silver III] Title: 1로 만들기, Time: 168 ms, Memory: 17460 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,31 +1,13 @@
 const fs = require("fs");
-const input = fs
-  .readFileSync(
-    process.platform === "linux" ? "/dev/stdin" : "./plus/input.txt"
-  )
-  .toString()
-  .trim()
-  .split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
 
-const num = Number(input[0]);
+const num = Number(input);
 const dp = Array(num + 1).fill(0);
 
-dp[1] = 0;
-dp[2] = 1;
-dp[3] = 1;
-if (num < 3) {
-  console.log(dp[num]);
-} else {
-  for (let i = 4; i <= num; i++) {
-    if (i % 3 === 0 && i % 2 === 0) {
-      dp[i] = Math.min(dp[i / 3], dp[i / 2], dp[i - 1]) + 1;
-    } else if (i % 3 === 0) {
-      dp[i] = Math.min(dp[i / 3], dp[i - 1]) + 1;
-    } else if (i % 2 === 0) {
-      dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
-    } else {
-      dp[i] = dp[i - 1] + 1;
-    }
-  }
-  console.log(dp[num]);
+for (let i = 2; i <= num; i++) {
+  dp[i] = dp[i - 1] + 1;
+  if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+  if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
 }
+
+console.log(dp[num]);
